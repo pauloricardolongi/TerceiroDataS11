2bb15b77dd58a280aa31974ce3b7cebcb34ddcd1
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-15 02:10:48 -0300

    horário brasileiro formatado/não formatado/UTC

--- src/curso/Programa.java
+++ src/curso/Programa.java
@@ -2,7 +2,9 @@ package curso;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Programa {
 
@@ -10,12 +12,33 @@ public class Programa {
 		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
 		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
+		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));//horário UTC
+		
+		
 		Date x1 = new Date(); 
+		Date x2 = new Date(System.currentTimeMillis());
+		Date x3 = new Date(0L);
+		Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));//horário brasil
 		
 		
-		
-		System.out.println("x1: " + sdf2.format(x1));
-		
+		System.out.println("-----------------------");
+		System.out.println("x1: " + sdf2.format(x1));//formatado
+		System.out.println("x2: " + sdf2.format(x2));
+		System.out.println("x3: " + sdf2.format(x3));
+		System.out.println("y3: " + sdf2.format(y3));
+
+		System.out.println("-----------------------");
+		System.out.println("x1: " + x1);//não formatado
+		System.out.println("x2: " + x2);
+		System.out.println("x3: " + x3);
+		System.out.println("y3: " + y3);
+
+		System.out.println("-----------------------");
+		System.out.println("x1: " + sdf3.format(x1));//UTC
+		System.out.println("x2: " + sdf3.format(x2));
+		System.out.println("x3: " + sdf3.format(x3));
+		System.out.println("y3: " + sdf3.format(y3));
 	}
 
 }
